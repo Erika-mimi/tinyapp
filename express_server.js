1622e3276f0e0db3e2b248f4c4b42250681df11d
Erika-mimi <erikayoshida5@gmail.com>
2021-12-09 21:44:52 +0000

    Created a registration handler

--- express_server.js
+++ express_server.js
@@ -5,13 +5,26 @@ const cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-  Math.random().toString(36).slice(7);
+  return Math.random().toString(36).slice(7);
 }
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -80,11 +93,37 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 app.get('/register', (req, res) => {
-  const templateVars = { username: req.cookies.username };
+  const templateVars = { username: req.cookies.user_id };
   res.render('urls_register', templateVars);
 });
 
 app.post('/register', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
+//Create a Registration Handler
+  if(!email || !password) return res.status(403).send('Wrong credentials')
+
+  const userObject = findUserByEmail(email, users)
+
+  if(userObject) return res.status(403).send('Email is taken')
+
+  const randomUserID = generateRandomString()
+  console.log('random user ID', randomUserID)
+  const newUser = {
+    id: randomUserID,
+    email: email,
+    password: password
+  }
+
+  users[randomUserID] = newUser
+  res.cookie('user_id', randomUserID)
+  res.redirect('/urls')
 });
+
+// returns user object if email match means user exists in userDB
+function findUserByEmail(email, userDB) {
+  for(const user in userDB) {
+    if(userDB[user].email === email) return userDB[user]
+  }
+  return false
+}
